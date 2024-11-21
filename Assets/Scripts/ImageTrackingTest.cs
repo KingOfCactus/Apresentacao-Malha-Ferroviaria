91a76d978f2905c110af8fccbc41c0ec177656a7
KingOfCactus <kingcactusgamedev@gmail.com>
2024-11-21 17:18:44 -0300

    Added more control to TrackingTest
    Added xbox 360 model for testing (https://sketchfab.com/3d-models/xbox-360-controller-31f699500e58427ebf042ff75abd4bec)

--- Assets/Scripts/ImageTrackingTest.cs
+++ Assets/Scripts/ImageTrackingTest.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
 
 [RequireComponent(typeof(ARTrackedImageManager))]
 public class ImageTrackingTest : MonoBehaviour
 {
+    [SerializeField] Slider maskScaleSlider;
+    [SerializeField] TextMeshProUGUI maskScaleText;
+
+    [SerializeField] Slider maskHeightSlider;
+    [SerializeField] TextMeshProUGUI maskHeightText;
+
+    [SerializeField] Slider modelHeightSlider;
+    [SerializeField] TextMeshProUGUI modelHeightText;
+
     ARTrackedImageManager trackingManager;
-    GameObject mask;
+    GameObject mask, model;
     
     void Start()
     {
@@ -22,5 +32,27 @@ public class ImageTrackingTest : MonoBehaviour
     } 
     
     public void ToogleTracking() => trackingManager.enabled = !trackingManager.enabled;
+    
+    public void UpdateMaskScale()
+    {
+        if (mask == null) return;
+        maskScaleText.text = maskScaleSlider.value.ToString();
+        mask.transform.localScale = Vector3.one * maskScaleSlider.value;
+    }
 
+    public void UpdateMaskHeight()
+    {
+        if (mask == null) return;
+        var oldPos = mask.transform.localPosition;
+        maskHeightText.text = maskHeightSlider.value.ToString();
+        mask.transform.localPosition = new Vector3(oldPos.x, maskHeightSlider.value, oldPos.z);
+    }
+
+    public void UpdateModelHeight()
+    {
+        if (model == null) model = GameObject.Find("Model");
+        var oldPos = model.transform.localPosition;
+        modelHeightText.text = modelHeightSlider.value.ToString();
+        model.transform.localPosition = new Vector3(oldPos.x, modelHeightSlider.value, oldPos.z);
+    }
 }
