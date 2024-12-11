92dd38b2623e1dd3934143f8c606091f4934bd55
KingOfCactus <kingcactusgamedev@gmail.com>
2024-12-11 06:41:20 -0300

    Replaces skybox with a sphere for 360º playback

--- Assets/Scripts/SphericalVideoPlayer.cs
+++ Assets/Scripts/SphericalVideoPlayer.cs
@@ -1,9 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.Video;
-using UnityEngine.UI;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.Video;
+using UnityEngine.Experimental.Rendering;
 
 [RequireComponent(typeof(VideoPlayer))]
 public class SphericalVideoPlayer : MonoBehaviour
@@ -15,11 +13,22 @@ public class SphericalVideoPlayer : MonoBehaviour
 	void PlayVideo(VideoClip _video)
 	{
 		texture = new RenderTexture(4096, 2048, 24);
+		texture.depthStencilFormat = GraphicsFormat.D16_UNorm;
 		RenderSettings.skybox.mainTexture = texture;
 		player.targetTexture = texture;
 		player.clip = _video;
 		player.Play();
 		// StartCoroutine("PreparePlayer");
+
+
+		// RenderTexture activeRenderTexture = RenderTexture.active;
+        // RenderTexture.active = camera.targetTexture;
+	    // camera.Render();
+        // Texture2D image = new Texture2D(renderTexture.width, renderTexture.height);
+        // image.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        // image.Apply();
+        // RenderTexture.active = activeRenderTexture;
+        // rawImage.texture = image;
 	}
 
 	IEnumerator PreparePlayer()
@@ -32,7 +41,7 @@ public class SphericalVideoPlayer : MonoBehaviour
 	void Awake()
 	{
 		player = GetComponent<VideoPlayer>();
-		PlayVideo(video);
+		// PlayVideo(video)
 
 	}
 
