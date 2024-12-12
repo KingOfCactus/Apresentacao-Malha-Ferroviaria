cffd0a8b4fc4edcba6441b8719c3c1a396a61894
KingOfCactus <kingcactusgamedev@gmail.com>
2024-12-12 09:30:26 -0300

    Improves VR presentation

--- Assets/Scripts/VRDirector.cs
+++ Assets/Scripts/VRDirector.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using UnityEngine.Video;
 using UnityEngine;
 
+[RequireComponent(typeof(SphericalVideoPlayer))]
 public class VRDirector : MonoBehaviour
-{
-    [SerializeField] VideoClip[] stationClips;
+{  
+    [System.Serializable]
+    public struct StationPlayback
+    {
+        public VideoClip clip;
+        public Vector3 sphereRotation;
+    }
+
+    [SerializeField] StationPlayback[] stations;
     SphericalVideoPlayer vrPlayer;
     
     void Start() => vrPlayer = GetComponent<SphericalVideoPlayer>();
+
     public void PlayStationVideo(int index)
     {
-        vrPlayer.Play360Video(stationClips[index]);
-        StartCoroutine("ResetWhenVideoEnds");
+        var playback = stations[index];
+        vrPlayer.Play360Video(playback.clip);
+        vrPlayer.transform.localEulerAngles = playback.sphereRotation;
+        StartCoroutine(ResetWhenVideoEnds((float)playback.clip.length));
     }
     
-    IEnumerator ResetWhenVideoEnds()
+    IEnumerator ResetWhenVideoEnds(float time)
     {
-        yield return new WaitForSeconds(1f);
-        yield return new WaitWhile(() => vrPlayer.isPlaying);
-        // SceneUtils.ReloadScene();
+        yield return new WaitForSeconds(time + 3f);
+        SceneUtils.ReloadScene();
     }
 }
